7b085784ea9c681fc8549d47a41c65fe3f97c855
samrithYoeun <samrith.yoeun@gmail.com>
2018-06-29 15:01:31 +0700

    first commit

---
+++ coffee shop/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  coffee shop
+//
+//  Created by PM Academy 3 on 6/29/18.
+//  Copyright © 2018 PM Academy 3. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ coffee shop/Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Extension.swift
+//  coffee shop
+//
+//  Created by PM Academy 3 on 6/29/18.
+//  Copyright © 2018 PM Academy 3. All rights reserved.
+//
+
+import Foundation
